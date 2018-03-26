43bbff4af981efb87d17344b09fe7aeacf0be0a5
Reggie Romano <romanoreggie@gmail.com>
2018-03-25 21:49:58 -0500

    established js to move racers

--- app.js
+++ app.js
@@ -12,12 +12,41 @@
 
 
 
-
+const track1 = []
+const track2 = []
 
 
 //right arrow moves racer 1 one space per keydown
-
-
-
+const movePlayerOne = () => {
+  document.getElementById("car1").nextElementSibling.setAttribute("id", "car1");
+  document.querySelector("#car1").removeAttribute("id");
+  track1.push("mile");
+  if (track1.length >= 14) {
+      alert("Player One Wins!");
+  }
+}
 
 //d key moves racer 2 one space per keydown
+const movePlayerTwo = () => {
+  document.getElementById("car2").nextElementSibling.setAttribute("id", "car2");
+  document.querySelector("#car2").removeAttribute("id");
+  track2.push("mile");
+  if (track2.length >= 14) {
+      alert("Player Two Wins!");
+  }
+}
+
+
+// Events for right arrow (39) and "D" (68) keys to movePlayer functions
+const moveRight = (event) => {
+  if (event.keyCode == 39) {
+    movePlayerOne();
+  }
+  if (event.keyCode == 68) {
+    movePlayerTwo();
+  }
+}
+
+
+// Event listener for keydown
+document.onkeydown = moveRight;
